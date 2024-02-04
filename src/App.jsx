1d70ebd35878c40c506708b1b8fb07cc04905455
JingKrit <kritsadal64@nu.ac.th>
2024-02-04 16:59:39 +0700

    edit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const url = "https://demo-backend-pi.vercel.app/";
+  const url = "https://demo-backend-pi.vercel.app";
   const [data, setData] = useState([]);
 
   const fetchInfo = () => {
